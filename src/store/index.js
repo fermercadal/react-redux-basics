dce225b5c54e353e305e5522ae743432ba0a0e5f
Fernando Mercadal <fermercadal@outlook.com>
2021-10-28 21:16:29 -0300

    Use spread

--- src/store/index.js
+++ src/store/index.js
@@ -5,28 +5,28 @@ const initialState = { counter: 0, showCounter: true };
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
+      ...state,
       counter: state.counter + 1,
-      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increase") {
     return {
+      ...state,
       counter: state.counter + action.amount,
-      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - 1,
-      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "toggle") {
     return {
-      counter: state.counter,
+      ...state,
       showCounter: !state.showCounter,
     }
   }
